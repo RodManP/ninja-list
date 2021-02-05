74b532ec841e71306125c81d009ade6efd562e69
Manuel <correo@empacotecnic.com>
2021-02-05 09:55:15 -0600

    initial

---
+++ pages/about.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+
+const About = () => {
+    return ( 
+        <>
+        <Head>
+           <title>Ninja List | About</title>
+           <meta name="keywords" content="ninjas"/>
+        </Head>
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis earum soluta magnam, repellat quasi eaque voluptates minima nesciunt quisquam optio cumque. Ipsum culpa commodi magni maxime cum dignissimos provident assumenda!
+            Possimus voluptate perferendis temporibus ab veniam sit repellat molestiae ratione iure officiis commodi, quae in suscipit pariatur sed alias ullam provident iusto tempora quisquam illo vitae dicta! Nihil, blanditiis maiores?
+            Voluptate fugit, exercitationem eveniet repudiandae id deserunt illo? Repudiandae labore sit quia nulla, aspernatur nesciunt, dolorum suscipit excepturi corporis autem ab, nihil non maiores obcaecati dolores nobis quam minus quas!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis earum soluta magnam, repellat quasi eaque voluptates minima nesciunt quisquam optio cumque. Ipsum culpa commodi magni maxime cum dignissimos provident assumenda!
+            Possimus voluptate perferendis temporibus ab veniam sit repellat molestiae ratione iure officiis commodi, quae in suscipit pariatur sed alias ullam provident iusto tempora quisquam illo vitae dicta! Nihil, blanditiis maiores?
+            Voluptate fugit, exercitationem eveniet repudiandae id deserunt illo? Repudiandae labore sit quia nulla, aspernatur nesciunt, dolorum suscipit excepturi corporis autem ab, nihil non maiores obcaecati dolores nobis quam minus quas!</p>
+        </div>
+        </>
+     );
+}
+ 
+export default About;
